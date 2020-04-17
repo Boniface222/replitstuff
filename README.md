12012c6aefd0aa7ed6fc397e43f9bb545e9f0558
Boniface <finigin222@gmail.com>
2020-04-17 13:47:09 -0300

    Experimental pygame based off a tutorial.
    Used url's for images which might get outdated.
    
    Thew caution to the wind.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Git repo to play around with Repl.it.
 
 [![Run on Repl.it](https://repl.it/badge/github/Boniface222/replitstuff)](https://repl.it/github/Boniface222/replitstuff)
+
+Experimental game based on https://www.pygame.org/docs/tut/ChimpLineByLine.html
+
--- main.py
+++ main.py
@@ -1,2 +1,180 @@
 # main.py
+# Based on https://www.pygame.org/docs/tut/ChimpLineByLine.html
+# Working version 1.0
+
+import os, sys, io
+from urllib.request import urlopen
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((199, 188))
+
+if not pygame.font: print('Warning, fonts disabled')
+if not pygame.mixer: print('Warning, sound disabled')
+
+url = "https://www.google.com/logos/doodles/2020/thank-you-packaging-shipping-and-delivery-workers-6753651837108760-s.png"
+soundurl = "http://soundbible.com/grab.php?id=2218&type=wav"
+imgstr = urlopen(url).read()
+imgfile = io.BytesIO(imgstr)
+dvdLogo = pygame.image.load(imgfile)
+print("test")
+
+
+def load_image(name, colorkey=None):
+    try:
+        imgstr = urlopen(name).read()
+        imgfile = io.BytesIO(imgstr)
+        image = pygame.image.load(imgfile)
+    except pygame.error as message:
+        print("Cannot load image:", name)
+        raise SystemExit(message)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    # fullname = os.path.join("data", name)
+    fullname = "C:\\Users\\Boniface\\Vim\\replitstuff\\data\\" + name
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error as message:
+        print('Cannot load sound:', wav)
+        raise SystemExit(message)
+    return sound
+
+
+class Fist(pygame.sprite.Sprite):
+    """moves a clenched fist on the screen, following the mouse"""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) # call Sprite initializer
+        self.image, self.rect = load_image(url, -1)
+        self.punching = 0
+
+    def update(self):
+        """move the fist pased on the mouse position"""
+        pos = pygame.mouse.get_pos()
+        self.rect.midtop = pos
+        if self.punching:
+            self.rect.move_ip(5, 10)
+
+    def punch(self, target):
+        """returns true if the fist collides with the target"""
+        if not self.punching:
+            self.punching = 1
+            hitbox = self.rect.inflate(-5, -5)
+            return hitbox.colliderect(target.rect)
+
+    def unpunch(self):
+        """called to pull the fist back"""
+        self.punching = 0
+
+class Chimp(pygame.sprite.Sprite):
+    """moves a monkey critter across the screen. it can spin the
+       monkey when it is punched."""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) # call Sprite initializer
+        self.image, self.rect = load_image(url, -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 10
+        self.move = 9
+        self.dizzy = 0
+
+    def update(self):
+        """walk or spin, depending on the monkeys state"""
+        if self.dizzy:
+            self._spin()
+        else:
+            self._walk()
+
+    def _walk(self):
+        """move the monkey across the screen, and turn at the ends"""
+        newpos = self.rect.move((self.move, 0))
+        # print(f"newpos = {newpos}")
+        # print(f"self.area.contains newpos?: \
+        #         {self.area.contains(newpos)}")
+        if not self.area.contains(newpos):
+            if self.rect.left < self.area.left or \
+                    self.rect.right > self.area.right:
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
+                self.image = pygame.transform.flip(self.image, 1, 0)
+        self.rect = newpos
+
+    def _spin(self):
+        """spin the monkey image"""
+        center = self.rect.center
+        self.dizzy += 12
+        if self.dizzy >= 360:
+            self.dizzy = 0
+            self.image = self.original
+        else:
+            rotate = pygame.transform.rotate
+            self.image = rotate(self.original, self.dizzy)
+        self.rect = self.image.get_rect(center=center)
+
+    def punched(self):
+        """this will cause the monkey to start spinning"""
+        if not self.dizzy:
+            self.dizzy = 1
+            self.original = self.image
+
+
+pygame.init()
+screen = pygame.display.set_mode((468, 60))
+pygame.display.set_caption("Monkey Fever")
+pygame.mouse.set_visible(0)
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((250, 250, 250))
+
+
+if pygame.font:
+    font = pygame.font.Font(None, 36)
+    text = font.render("Pummel The Chimp, And Win $$$", 1, (10, 10, 10))
+    textpos = text.get_rect(centerx=int(background.get_width()/2))
+    background.blit(text,textpos)
+
+
+screen.blit(background, (0, 0))
+pygame.display.flip()
+
+whiff_sound = load_sound("whiff.wav")
+punch_sound = load_sound("punch.wav")
+chimp = Chimp()
+fist = Fist()
+allsprites = pygame.sprite.RenderPlain((fist, chimp))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            running = False
+        elif event.type == MOUSEBUTTONDOWN:
+            if fist.punch(chimp):
+                punch_sound.play()  # punch
+                chimp.punched()
+            else:
+                whiff_sound.play()  # miss
+        elif event.type == MOUSEBUTTONUP:
+            fist.unpunch()
+
+    allsprites.update()
+    screen.blit(background, (0, 0))
+    allsprites.draw(screen)
+    pygame.display.flip()
 
